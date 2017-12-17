4c668b1cef58a8b57d2d66d3dfb633f4845b79d2
Hawthorn2013 <mynetuser@outlook.com>
2017-12-17 22:34:22 +0800

    Refactor CMD Interface of Bilibili Download

--- danmu_diyidan_download.py
+++ danmu_diyidan_download.py
@@ -1,12 +1,13 @@
 import urllib.request
 
 
-def danmu_download(id):
-    url = 'https://www.diyidan.com/main/post/danmu?postId=%s' % id
-    try:
-        with urllib.request.urlopen(url) as f:
-            danmu = f.read()
-    except Exception as e:
-        print(e)
-        return None
-    return danmu
+class danmu_diyidan_download(object):
+    def danmu_download(self, id):
+        url = 'https://www.diyidan.com/main/post/danmu?postId=%s' % id
+        try:
+            with urllib.request.urlopen(url) as f:
+                danmu = f.read()
+        except Exception as e:
+            print(e)
+            return None
+        return danmu
