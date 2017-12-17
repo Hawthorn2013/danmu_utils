6d07c03ea584977d1fc1ad7aea24437b7523aeba
Hawthorn2013 <mynetuser@outlook.com>
2017-12-17 15:53:53 +0800

    Refector
    - Split danmu entry process.
    - Ignore error danmu entry.

--- danmu_bilibili_out.py
+++ danmu_bilibili_out.py
@@ -3,25 +3,20 @@ from xml.dom import minidom
 
 def danmu_out(danmu_common):
     xml = minidom.Document()
-    root = xml.createElement('i')
-    xml.appendChild(root)
-    # node = xml.createElement('chatserver')
-    # node.appendChild(xml.createTextNode('chat.bilibili.com'))
-    # root.appendChild(node)
-    # node = xml.createElement('chatid')
-    # node.appendChild(xml.createTextNode('0'))
-    # root.appendChild(node)
-    # node = xml.createElement('mission')
-    # node.appendChild(xml.createTextNode('0'))
-    # root.appendChild(node)
-    # node = xml.createElement('maxlimit')
-    # node.appendChild(xml.createTextNode('3000'))
-    # root.appendChild(node)
-    # node = xml.createElement('source')
-    # node.appendChild(xml.createTextNode('e-r'))
-    # root.appendChild(node)
+    list = xml.createElement('i')
+    xml.appendChild(list)
     for entry_common in danmu_common['list']:
-        node = xml.createElement('d')
+        entry_bilibili = entry_out(entry_common)
+        if (entry_bilibili == None):
+            continue
+        list.appendChild(entry_bilibili)
+    danmu_bilibili = xml.toprettyxml(encoding='UTF-8')
+    return danmu_bilibili
+
+def entry_out(entry_common):
+    xml = minidom.Document()
+    try:
+        entry_bilibili = xml.createElement('d')
         p = '%f,%d,%d,%d,%d,%d,%d,%d' % (
             entry_common['time'],  # 1. 弹幕发送相对视频的时间（以前是以秒为单位的整数，现在用浮点记了，更精准）
             1,  # 2. 弹幕类型：1~3（但貌似只见过1）滚动弹幕、4底端弹幕、5顶端弹幕、6逆向弹幕、7精准定位、8高级弹幕【默认是1，基本以1、4、5多见】
@@ -30,11 +25,11 @@ def danmu_out(danmu_common):
             0,  # 5. 弹幕发送时的UNIX时间戳，基准时间1970-1-1 08:00:00
             0,  # 6. 弹幕池类型：0普通 1字幕 2特殊
             entry_common['sender'],  # 7. 发送者ID【注意不是uid，具体怎么关联的还不清楚，屏蔽用】
-            8  # 8. 弹幕在数据库的ID
+            0  # 8. 弹幕在数据库的ID
         )
-        node.setAttribute('p', p)
-        node.appendChild(xml.createTextNode(entry_common['text']))
-        root.appendChild(node)
-
-    danmu_bilibili = xml.toprettyxml(encoding='UTF-8')
-    return danmu_bilibili
+        entry_bilibili.setAttribute('p', p)
+        entry_bilibili.appendChild(xml.createTextNode(entry_common['text']))
+    except Exception as e:
+        print(e)
+        return None
+    return entry_bilibili
--- danmu_diyidan_in.py
+++ danmu_diyidan_in.py
@@ -2,13 +2,27 @@ import json
 
 
 def danmu_in(danmu):
-    danmu_diyidan = json.loads(danmu)
+    try:
+        danmu_diyidan = json.loads(danmu)
+    except Exception as e:
+        print(e)
+        return None
     danmu_common = {'list': []}
-    for entry_diyidan in danmu_diyidan['danmu']['danmakuList']:
-        entry_common = {}
-        entry_common['text'] = entry_diyidan['text']
-        entry_common['time'] = entry_diyidan['time'] / 10
-        entry_common['color'] = int(entry_diyidan['color'].split('#')[1], 16)
-        entry_common['sender'] = entry_diyidan['danmakuId']
+    for entry_diyidan in danmu_diyidan['data']['danmakuList']:
+        entry_common = entry_in(entry_diyidan)
+        if (entry_common == None):
+            continue
         danmu_common['list'].append(entry_common)
     return danmu_common
+
+def entry_in(entry):
+    entry_common = {}
+    try:
+        entry_common['text'] = entry['text']
+        entry_common['time'] = entry['time'] / 10
+        entry_common['color'] = int(entry['color'].split('#')[1], 16)
+        entry_common['sender'] = entry['danmakuId']
+    except Exception as e:
+        print(e)
+        return None
+    return entry_common
