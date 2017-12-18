4090e7e3551a90ccd1a13d3ca2f8e1c559b32a8f
Hawthorn2013 <mynetuser@outlook.com>
2017-12-18 23:38:22 +0800

    Refactor

--- danmu_bilibili_download.py
+++ danmu_bilibili_download.py
@@ -3,23 +3,45 @@ import zlib
 
 
 class danmu_bilibili_download(object):
-    def deflate(self, data):
+    @staticmethod
+    def deflate(data):
         try:
             return zlib.decompress(data, -zlib.MAX_WBITS)
         except zlib.error:
             return zlib.decompress(data)
 
-    def danmu_download(self, id):
-        url = 'https://comment.bilibili.com/%s.xml' % id
-        try:
-            with urllib.request.urlopen(url) as f:
-                danmu = f.read()
-        except Exception as e:
-            print(e)
-            return None
-        try:
-            danmu_deflate = self.deflate(danmu)
-        except Exception as e:
-            print(e)
-            return None
-        return danmu_deflate
+    @staticmethod
+    def danmu_download(aid=None):
+        res = {}
+        if aid != None:
+            url = 'https://comment.bilibili.com/%s.xml' % (aid)
+            try:
+                with urllib.request.urlopen(url) as f:
+                    danmu = f.read()
+                try:
+                    danmu_deflate = danmu_bilibili_download.deflate(danmu)
+                    res['aid'] = danmu_deflate
+                except Exception as e:
+                    print(e)
+            except Exception as e:
+                print(e)
+        return res
+
+    @staticmethod
+    def danmu_download_file(line):
+        line_params = line.strip('\n').split('\t')
+        print('Start process item: "%s".' % line.strip('\n'))
+        if len(line_params) < 1:
+            return False
+        if not line_params[0].isdecimal():
+            return False
+        aid = line_params[0]
+        res = danmu_bilibili_download.danmu_download(aid=aid)
+        if 'aid' in res:
+            out_filename = '%s.%s' % (aid, 'xml')
+            try:
+                with open(out_filename, mode='wb') as f:
+                    f.write(res['aid'])
+                print('Success generate file: "%s".' % out_filename)
+            except Exception as e:
+                print(e)
--- download_bilibili.py
+++ download_bilibili.py
@@ -1,21 +1,21 @@
 # !/usr/bin/python3
 import sys
 import os
-import danmu_download
 import danmu_bilibili_download
+import cmdparam_download
 
 if __name__ == '__main__':
-    danmu_download_1 = danmu_download.danmu_download(danmu_bilibili_download.danmu_bilibili_download(), 'xml',
-                                                     'xmllist')
+    danmu_download_1 = cmdparam_download.cmdparam_download(danmu_bilibili_download.danmu_bilibili_download(), 'xml',
+                                                           'xmllist')
     if len(sys.argv) > 1:
         for i in range(1, len(sys.argv)):
             argv_item = sys.argv[i]
-            if argv_item.isdecimal():
-                danmu_download_1.download_id(argv_item)
-            elif os.path.isfile(argv_item):
+            if os.path.isfile(argv_item):
                 danmu_download_1.download_file(argv_item)
             elif os.path.isdir(argv_item):
                 danmu_download_1.download_dir(argv_item)
+            else:
+                danmu_download_1.download_item(argv_item)
     else:
         danmu_download_1.download_dir('.')
     os.system('pause')
