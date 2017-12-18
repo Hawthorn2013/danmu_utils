b9aa79430af8caab8ba2eedd7a2a08d6a3a01fac
Hawthorn2013 <mynetuser@outlook.com>
2017-12-18 21:55:26 +0800

    Add New Function

--- danmu_diyidan_download.py
+++ danmu_diyidan_download.py
@@ -16,3 +16,57 @@ class danmu_diyidan_download(object):
             print(e)
             return None
         return danmu
+
+    @staticmethod
+    def danmu_download(videoId=None, postId=None):
+        res = {}
+        if videoId != None:
+            url = 'https://api.diyidan.net/v0.2/posts/danmaku?videoId=%s' % (videoId)
+            try:
+                with urllib.request.urlopen(url) as f:
+                    danmu = f.read()
+                    res['videoId'] = danmu
+            except Exception as e:
+                print(e)
+        if postId != None:
+            url = 'https://api.diyidan.net/v0.2/posts/danmaku?postId=%s' % (postId)
+            try:
+                with urllib.request.urlopen(url) as f:
+                    danmu = f.read()
+                    res['postId'] = danmu
+            except Exception as e:
+                print(e)
+        return res
+
+    @staticmethod
+    def danmu_download_file(line):
+        line_params = line.strip('\n').split('\t')
+        print('Start process item: "%s".' % line)
+        if len(line_params) < 2:
+            return False
+        if not line_params[0].isdecimal():
+            return False
+        if not line_params[1].isdecimal():
+            return False
+        videoId = line_params[0]
+        postId = line_params[1]
+        res = danmu_diyidan_download.danmu_download(videoId, postId)
+        if 'videoId' in res:
+            if postId != None:
+                out_filename = '%s-%s.%s' % (postId, videoId, 'dydjson')
+            else:
+                out_filename = '-%s.%s' % (videoId, 'dydjson')
+            try:
+                with open(out_filename, mode='wb') as f:
+                    f.write(res['videoId'])
+                print('Success generate file: "%s".' % out_filename)
+            except Exception as e:
+                print(e)
+        if 'postId' in res:
+            out_filename = '%s-.%s' % (postId, 'dydjson')
+            try:
+                with open(out_filename, mode='wb') as f:
+                    f.write(res['postId'])
+                print('Success generate file: "%s".' % out_filename)
+            except Exception as e:
+                print(e)
