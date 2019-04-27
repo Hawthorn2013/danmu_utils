67ecb9a791dce1297920f386d02819562b287554
Hawthorn2013 <hawthorn7dd@hotmail.com>
2019-04-27 13:48:04 +0800

    Amend Exception Handle

--- danmu_utils/common/convert_helper.py
+++ danmu_utils/common/convert_helper.py
@@ -6,27 +6,18 @@ __all__ = ['convert_file', 'convert_dir']
 
 def convert_file(filename, tool):
     print('Start process file: "%s".' % filename)
-    try:
-        with open(filename, encoding='utf8') as f:
-            danmu_diyidan = f.read()
-    except Exception as e:
-        print(e)
-        return False
+    with open(filename, encoding='utf8') as f:
+        danmu_diyidan = f.read()
     # Determine output file name
     tmp_1 = os.path.splitext(filename)
     if tmp_1[1] == '.' + tool.DANMU_EXTNAME_SRC:
         out_filename = tmp_1[0] + '.' + tool.DANMU_EXTNAME_DST
     else:
         out_filename = filename + '.' + tool.DANMU_EXTNAME_DST
-    try:
-        danmu_out = tool.convert(danmu_diyidan)
-        with open(out_filename, mode='wb') as f:
-            f.write(danmu_out)
-    except Exception as e:
-        print(e)
-        return False
+    danmu_out = tool.convert(danmu_diyidan)
+    with open(out_filename, mode='wb') as f:
+        f.write(danmu_out)
     print('Success generate file: "%s".' % out_filename)
-    return True
 
 
 def convert_dir(dirname, tool):
@@ -36,4 +27,8 @@ def convert_dir(dirname, tool):
             continue
         if os.path.splitext(filename)[1] != '.' + tool.DANMU_EXTNAME_SRC:
             continue
-        convert_file(filename, tool)
+        try:
+            convert_file(filename, tool)
+        except Exception as e:
+            print(e)
+            continue
