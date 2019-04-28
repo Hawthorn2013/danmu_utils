a29c65032cb85fe189639b3ddbe462db4d961c62
Hawthorn2013 <hawthorn7dd@hotmail.com>
2019-04-28 23:02:47 +0800

    Add Output Dir Argument Support

--- danmu_utils/common/cmd_interface.py
+++ danmu_utils/common/cmd_interface.py
@@ -14,23 +14,26 @@ if __name__ == '__main__':
     parser.add_argument('--dst-danmu-type')
     parser.add_argument('--add-dir-timestamp', action='store_true')
     parser.add_argument('--add-file-timestamp', action='store_true')
+    parser.add_argument('--output-dir')
     args = parser.parse_args()
     if args.action == 'download':
         download_tool = get_download_tool(args.danmu_type)()
         if args.data_type == 'line':
-            download_line(args.data_value, download_tool)
+            download_line(args.data_value, download_tool, output_dir=args.output_dir)
         elif args.data_type == 'file':
             download_file(args.data_value, download_tool, add_dir_timestamp=args.add_dir_timestamp,
+                          output_dir=args.output_dir,
                           add_file_timestamp=args.add_file_timestamp)
         elif args.data_type == 'dir':
             download_dir(args.data_value, download_tool, add_dir_timestamp=args.add_dir_timestamp,
+                         output_dir=args.output_dir,
                          add_file_timestamp=args.add_file_timestamp)
     elif args.action == 'convert':
         convert_tool = get_convert_tool(args.src_danmu_type, args.dst_danmu_type)()
         if args.data_type == 'file':
-            convert_file(args.data_value, convert_tool)
+            convert_file(args.data_value, convert_tool, output_dir=args.output_dir)
         elif args.data_type == 'dir':
-            convert_dir(args.data_value, convert_tool)
+            convert_dir(args.data_value, convert_tool, output_dir=args.output_dir)
         else:
             print('Illegal param: --data-type=%s' % args.data_type)
             exit(1)
--- danmu_utils/common/convert_helper.py
+++ danmu_utils/common/convert_helper.py
@@ -4,7 +4,7 @@ import os
 __all__ = ['convert_file', 'convert_dir']
 
 
-def convert_file(filename, tool):
+def convert_file(filename, tool, output_dir=None):
     print('Start process file: "%s".' % filename)
     with open(filename, encoding='utf8') as f:
         danmu_diyidan = f.read()
@@ -15,12 +15,16 @@ def convert_file(filename, tool):
     else:
         out_filename = filename + '.' + tool.DANMU_EXTNAME_DST
     danmu_out = tool.convert(danmu_diyidan)
+    if output_dir != None:
+        if not os.path.exists(output_dir):
+            os.mkdir(output_dir)
+        out_filename = os.path.join(output_dir, os.path.split(out_filename)[1])
     with open(out_filename, mode='wb') as f:
         f.write(danmu_out)
     print('Success generate file: "%s".' % out_filename)
 
 
-def convert_dir(dirname, tool):
+def convert_dir(dirname, tool, output_dir=None):
     for filename in os.listdir(dirname):
         filename = os.path.join(dirname, filename)
         if not os.path.isfile(filename):
@@ -28,7 +32,7 @@ def convert_dir(dirname, tool):
         if os.path.splitext(filename)[1] != '.' + tool.DANMU_EXTNAME_SRC:
             continue
         try:
-            convert_file(filename, tool)
+            convert_file(filename, tool, output_dir=output_dir)
         except Exception as e:
             print(e)
             continue
